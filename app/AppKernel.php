a604034fb2381316deba106d7a3a920deaa84ad3
Alphonse De Doge <queutcha@gmail.com>
2017-12-02 18:22:43 +0100

    Ajout menu

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new screenAddictBundle\screenAddictBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- composer_requires.bat
+++ composer_requires.bat
@@ -3,3 +3,4 @@ composer require symfony/assetic-bundle
 composer require components/jquery
 composer require oyejorge/less.php
 php bin/console assetic:dump
+composer require knplabs/knp-menu-bundle "^2.0"
