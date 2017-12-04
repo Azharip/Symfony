dd74b23318d45aa54c021434d02523b599153817
Alphonse De Doge <queutcha@gmail.com>
2017-12-04 14:10:48 +0100

    continuation ajax

--- src/screenAddictBundle/Controller/DefaultController.php
+++ src/screenAddictBundle/Controller/DefaultController.php
@@ -131,10 +131,26 @@ class DefaultController extends Controller
 
     public function rechercherAction(Request $request)
     {
+        /*$recherche = "mars";
+        $url = "https://api.themoviedb.org/3/search/movie?api_key=5cac0300f480fa473ca2b57296132a8f&language=en-US&query=".$recherche."&page=1&include_adult=false";
+        $json_source = file_get_contents($url);
+        $json_data = json_decode($json_source);
+        $i = 0;
+        foreach($json_data as $d)
+        {
+            if($i < 10)
+                $results[$i] = $d['results'];
+            $i++;
+        }
 
-        if($request->request->get('some_var_name')){
+        echo("<pre>");
+        var_dump($results);
+        echo("</pre>");*/
+
+        if($request->request->get('test1')){
             //make something curious, get some unbelieveable data
-            $arrData = ['output' => 'here the result which will appear in div'];
+
+            $arrData = ['output' => 'atchoum'];
             return new JsonResponse($arrData);
         }
 
